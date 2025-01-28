aad1506d8dd32c21c45ac5278baea315795bed16
anandsonwane <123882119+anandsonwane@users.noreply.github.com>
2025-01-28 10:52:39 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
 print("Hello Anand")
 print("Learining Jenkins")
 print("Adding automation in it using poll hooks but in production Hooks are used which is 4th Option  but our jenkins is not publice that's why it cannot be created but company uses this")
-pri
